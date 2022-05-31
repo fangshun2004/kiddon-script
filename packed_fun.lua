ffc2dc9b4a3ad4de5562bd564b7d8e2a11de8260
fangshun <fangshun2004@msn.com>
2022-05-31 23:15:29 +0800

    1.增加packed 函数模块 1.Added packed function module 2.增加中文菜单 2.Added Chinese menu

---
+++ packed_fun.lua
@@ -0,0 +1,325 @@
+﻿local packed_funs = {}
+
+local mpx = "MP" .. stats.get_int("MPPLY_LAST_MP_CHAR") .. "_"
+
+local function get_packed_bool_date(index, mpx)
+    local statName = nil
+    -- 0-198
+    if (index >= 0 and index < 192) then
+        i = (index - 0) // 64
+        index = (index - 0) % 64
+        statName = mpx .. "PSTAT_BOOL" .. i
+    end
+    -- --513-705
+    if (index >= 513 and index < 705) then
+        i = (index - 513) // 64
+        index = (index - 513) % 64
+        statName = "MP_PSTAT_BOOL" .. i
+    end
+    -- 2919-3111
+    if (index >= 2919 and index < 3111) then
+        i = (index - 2919) // 64
+        index = (index - 2919) % 64
+        statName = "MP_TUPSTAT_BOOL" .. i
+    end
+    ----3111-3879
+    if (index >= 3111 and index < 3879) then
+        i = (index - 3111) // 64
+        index = (index - 3111) % 64
+        statName = mpx .. "TUPSTAT_BOOL" .. i
+    end
+    -- --4207--4335  
+    if (index >= 4207 and index < 4335) then
+        i = (index - 4207) // 64
+        index = (index - 4207) % 64
+        statName = mpx .. "NGPSTAT_BOOL" .. i
+    end
+    -- 4335-4399
+    if (index >= 4335 and index < 4399) then
+        i = (index - 4335) // 64
+        index = (index - 4335) % 64
+        statName = "MP_NGPSTAT_BOOL" .. i
+    end
+    -- 6029-6413
+    if (index >= 6029 and index < 6413) then
+        i = (index - 6029) // 64
+        index = (index - 6029) % 64
+        statName = mpx .. "NGTATPSTAT_BOOL" .. i
+    end
+    -- 7321-7385
+    if (index >= 7321 and index < 7385) then
+        i = (index - 7321) // 64
+        index = (index - 7321) % 64
+        statName = "MP_NGDLCPSTAT_BOOL" .. i
+    end
+    -- 7385--7641
+    if (index >= 7385 and index < 7641) then
+        i = (index - 7385) // 64
+        index = (index - 7385) % 64
+        statName = mpx .. "NGDLCPSTAT_BOOL" .. i
+    end
+    -- 9361-9553
+    if (index >= 9361 and index < 9553) then
+        i = (index - 9361) // 64
+        index = (index - 9361) % 64
+        statName = mpx .. "DLCBIKEPSTAT_BOOL" .. i
+    end
+    -- 15369-15561
+    if (index >= 15369 and index < 15561) then
+        i = (index - 15369) // 64
+        index = (index - 15369) % 64
+        statName = mpx .. "DLCGUNPSTAT_BOOL" .. i
+    end
+    ----15562-15946
+    if (index >= 15562 and index < 15946) then
+        i = (index - 15562) // 64
+        index = (index - 15562) % 64
+        statName = mpx .. "GUNTATPSTAT_BOOL" .. i
+    end
+    -- 15946-16010
+    if (index >= 15946 and index < 16010) then
+        i = (index - 15946) // 64
+        index = (index - 15946) % 64
+        statName = mpx .. "DLCSMUGCHARPSTAT_BOOL" .. i
+    end
+    -- 18098-18162
+    if (index >= 18098 and index < 18162) then
+        i = (index - 18098) // 64
+        index = (index - 18098) % 64
+        statName = mpx .. "GANGOPSPSTAT_BOOL" .. i
+    end
+    ----22066-22194
+    if (index >= 22066 and index < 22194) then
+        i = (index - 22066) // 64
+        index = (index - 22066) % 64
+        statName = mpx .. "BUSINESSBATPSTAT_BOOL" .. i
+    end
+    -- 24962-25538
+    if (index >= 24962 and index < 25538) then
+        i = (index - 24962) // 64
+        index = (index - 24962) % 64
+        statName = mpx .. "ARENAWARSPSTAT_BOOL" .. i
+    end
+    -- 26810-27258
+    if (index >= 26810 and index < 27258) then
+        i = (index - 26810) // 64
+        index = (index - 26810) % 64
+        statName = mpx .. "CASINOPSTAT_BOOL" .. i
+    end
+    -- 28098-28354
+    if (index >= 28098 and index < 28354) then
+        i = (index - 28098) // 64
+        index = (index - 28098) % 64
+        statName = mpx .. "CASINOHSTPSTAT_BOOL" .. i
+    end
+    -- 28355-28483
+    if (index >= 28355 and index < 28483) then
+        i = (index - 28355) // 64
+        index = (index - 28355) % 64
+        statName = mpx .. "HEIST3TATTOOSTAT_BOOL" .. i
+    end
+    -- 30227-30355
+    if (index >= 30227 and index < 30355) then
+        i = (index - 30227) // 64
+        index = (index - 30227) % 64
+        statName = mpx .. "SU20PSTAT_BOOL" .. i
+    end
+    -- 30355-30483
+    if (index >= 30355 and index < 30483) then
+        i = (index - 30355) // 64
+        index = (index - 30355) % 64
+        statName = mpx .. "SU20TATTOOSTAT_BOOL" .. i
+    end
+    -- 30515-30707
+    if (index >= 30515 and index < 30707) then
+        i = (index - 30515) // 64
+        index = (index - 30515) % 64
+        statName = mpx .. "HISLANDPSTAT_BOOL" .. i
+    end
+    -- 31707-32283
+    if (index >= 31707 and index < 32283) then
+        i = (index - 31707) // 64
+        index = (index - 31707) % 64
+        statName = mpx .. "TUNERPSTAT_BOOL" .. i
+    end
+    -- 32283-32411
+    if (index >= 32283 and index < 32411) then
+        i = (index - 32283) // 64
+        index = (index - 32283) % 64
+        statName = mpx .. "FIXERPSTAT_BOOL" .. i
+    end
+    -- 32411-32475
+    if (index >= 32411 and index < 32475) then
+        i = (index - 32411) // 64
+        index = (index - 32411) % 64
+        statName = mpx .. "FIXERTATTOOSTAT_BOOL" .. i
+    end
+    return statName, index
+end
+
+function packed_funs.set_packed_bool(index, value)
+    local statname
+    local bitindex
+    statname, bitindex = get_packed_bool_date(index, mpx)
+    print("statname is:", statname, "\nbitindex is:", bitindex)
+    if (statname ~= nil) then
+        stats.set_bool_masked(statname, value, bitindex)
+    end
+end
+
+function packed_funs.get_packed_bool(index)
+    local statname
+    local bitindex
+    statname, bitindex = get_packed_bool_date(index, mpx)
+    if (statname ~= nil) then
+        return stats.get_bool_masked(statname, bitindex)
+    end
+end
+
+local function get_packed_int_date(index, mpx)
+    local statName = nil
+    if (index >= 384 and index < 457) then
+        i = (index - 384) // 8
+        index = (index - 384) % 8 * 8
+        statName = mpx .. "PSTAT_INT" .. i
+    end
+    if (index >= 1281 and index < 1305) then
+        i = (index - 1281) // 8
+        index = (index - 1281) % 8 * 8
+        statName = "MP_PSTAT_INT" .. i
+    end
+    if (index >= 1361 and index < 1393) then
+        i = (index - 1361) // 8
+        index = (index - 1361) % 8 * 8
+        statName = "MP_TUPSTAT_INT" .. i
+    end
+    if (index >= 1393 and index < 2919) then
+        i = (index - 1393) // 8
+        index = (index - 1393) % 8 * 8
+        statName = mpx .. "TUPSTAT_INT" .. i
+    end
+    if (index >= 4143 and index < 4207) then
+        i = (index - 4143) // 8
+        index = (index - 4143) % 8 * 8
+        statName = "MP_NGPSTAT_INT" .. i
+    end
+    if (index >= 3879 and index < 4143) then
+        i = (index - 3879) // 8
+        index = (index - 3879) % 8 * 8
+        statName = mpx .. "NGPSTAT_INT" .. i
+    end
+    if (index >= 4399 and index < 6028) then
+        i = (index - 4399) // 8
+        index = (index - 4399) % 8 * 8
+        statName = mpx .. "LRPSTAT_INT" .. i
+    end
+    if (index >= 6413 and index < 7262) then
+        i = (index - 6413) // 8
+        index = (index - 6413) % 8 * 8
+        statName = mpx .. "APAPSTAT_INT" .. i
+    end
+    if (index >= 7262 and index < 7313) then
+        i = (index - 7262) // 8
+        index = (index - 7262) % 8 * 8
+        statName = mpx .. "LR2PSTAT_INT" .. i
+    end
+    if (index >= 7313 and index < 7321) then
+        i = (index - 7313) // 8
+        index = (index - 7313) % 8 * 8
+        statName = "MP_NGDLCPSTAT_INT" .. i
+    end
+    if (index >= 7641 and index < 7681) then
+        i = (index - 7641) // 8
+        index = (index - 7641) % 8 * 8
+        statName = mpx .. "NGDLCPSTAT_INT" .. i
+    end
+    if (index >= 7681 and index < 9361) then
+        i = (index - 7681) // 8
+        index = (index - 7681) % 8 * 8
+        statName = mpx .. "BIKEPSTAT_INT" .. i
+    end
+    if (index >= 9553 and index < 15265) then
+        i = (index - 9553) // 8
+        index = (index - 9553) % 8 * 8
+        statName = mpx .. "IMPEXPPSTAT_INT" .. i
+    end
+    if (index >= 15265 and index < 15369) then
+        i = (index - 15265) // 8
+        index = ((index - 15265) % 8) * 8
+        statName = mpx .. "GUNRPSTAT_INT" .. i
+    end
+    if (index >= 16010 and index < 18098) then
+        i = (index - 16010) // 8
+        index = (index - 16010) % 8 * 8
+        statName = mpx .. "DLCSMUGCHARPSTAT_INT" .. i
+    end
+    if (index >= 18162 and index < 19018) then
+        i = (index - 18162) // 8
+        index = (index - 18162) % 8 * 8
+        statName = mpx .. "GANGOPSPSTAT_INT" .. i
+    end
+    if (index >= 19018 and index < 22066) then
+        i = (index - 19018) // 8
+        index = ((index - 19018) % 8) * 8
+        statName = mpx .. "BUSINESSBATPSTAT_INT" .. i
+    end
+    if (index >= 22194 and index < 24962) then
+        i = (index - 22194) // 8
+        index = (index - 22194) % 8 * 8
+        statName = mpx .. "ARENAWARSPSTAT_INT" .. i
+    end
+    if (index >= 25538 and index < 26810) then
+        i = (index - 25538) // 8
+        index = (index - 25538) % 8 * 8
+        statName = mpx .. "CASINOPSTAT_INT" .. i
+    end
+    if (index >= 27258 and index < 28098) then
+        i = (index - 27258) // 8
+        index = (index - 27258) % 8 * 8
+        statName = mpx .. "CASINOHSTPSTAT_INT" .. i
+    end
+    if (index >= 28483 and index < 30227) then
+        i = (index - 28483) // 8
+        index = (index - 28483) % 8 * 8
+        statName = mpx .. "SU20PSTAT_INT" .. i
+    end
+    if (index >= 30483 and index < 30515) then
+        i = (index - 30483) // 8
+        index = (index - 30483) % 8 * 8
+        statName = mpx .. "HISLANDPSTAT_INT" .. i
+    end
+    if (index >= 30707 and index < 31707) then
+        i = (index - 30707) // 8
+        index = (index - 30707) % 8 * 8
+        statName = mpx .. "TUNERPSTAT_INT" .. i
+    end
+    if (index >= 32475 and index < 34123) then
+        i = (index - 32475) // 8
+        index = (index - 32475) % 8 * 8
+        statName = mpx .. "FIXERPSTAT_INT" .. i
+    end
+    return statName, index
+end
+
+function packed_funs.set_packed_int(index, value)
+    local statname
+    local bitStart
+
+    statname, bitStart = get_packed_int_date(index, mpx)
+    print("statname is:", statname, "\nbitindex is:", bitStart)
+    if (statname ~= nil) then
+        stats.set_masked_int(statname, value, bitStart, 8)
+        print("INT writw done")
+    end
+end
+
+function packed_funs.get_packed_int(index)
+    local statname
+    local bitStart
+    statname, bitindex = get_packed_int_date(index, mpx)
+    if (statname ~= nil) then
+        return stats.get_masked_int(statname, bitStart, 8)
+    end
+end
+
+return packed_funs
---
+++ packed_unlock_cn.lua
@@ -0,0 +1,196 @@
+﻿local packed_funs = require("./scripts/packed_fun") -- 调用packed_fun文件
+
+local packed_unlock_menu = menu.add_submenu("packed 解锁") -- 一级菜单
+
+local packed_bool_unlock_menu = packed_unlock_menu:add_submenu("Packed bool 解锁") -- 二级菜单
+
+local function set_mass_packed_bool(value, min_i, max_i)
+    for index = min_i, max_i do
+        packed_funs.set_packed_bool(index, value)
+    end
+end
+
+-------------------------------------解锁/锁定切换按钮-------------------------------------
+local unlock_state = true
+packed_bool_unlock_menu:add_toggle("-------------------解锁/锁定", function()
+    return unlock_state
+end, function()
+    unlock_state = not unlock_state
+end)
+-------------------------------------packed bool unlock-------------------------------------
+
+packed_bool_unlock_menu:add_action("PSTAT_BOOL", function() -- 0-192
+    set_mass_packed_bool(unlock_state, 0, 192)
+    packed_funs.set_packed_bool(24, false) -- 迷你地图不放大
+    packed_funs.set_packed_bool(158, false) -- 免费象鼻虫 free weevil
+end)
+
+packed_bool_unlock_menu:add_action("MP_PSTAT_BOOL", function() -- 513-705
+    set_mass_packed_bool(unlock_state, 513, 705)
+end)
+
+packed_bool_unlock_menu:add_action("MP_TUPSTAT_BOOL", function() -- 2919-3111
+    set_mass_packed_bool(unlock_state, 2919, 3111)
+end)
+
+packed_bool_unlock_menu:add_action("TUPSTAT_BOOL", function() -- 3111-3879
+    set_mass_packed_bool(unlock_state, 3111, 3879)
+end)
+
+packed_bool_unlock_menu:add_action("NGPSTAT_BOOL", function() -- 4207-4335
+    set_mass_packed_bool(unlock_state, 4207, 4335)
+end)
+
+packed_bool_unlock_menu:add_action("MP_NGPSTAT_BOOL", function() -- 4335-4399
+    set_mass_packed_bool(unlock_state, 4335, 4399)
+end)
+
+packed_bool_unlock_menu:add_action("NGTATPSTAT_BOOL", function() -- 6029-6413
+    set_mass_packed_bool(unlock_state, 6029, 6413)
+end)
+
+packed_bool_unlock_menu:add_action("MP_NGDLCPSTAT_BOOL", function() -- 7321-7385
+    set_mass_packed_bool(unlock_state, 7321, 7385)
+end)
+
+packed_bool_unlock_menu:add_action("NGDLCPSTAT_BOOL", function() -- 7385-7641
+    set_mass_packed_bool(unlock_state, 7385, 7641)
+end)
+
+packed_bool_unlock_menu:add_action("DLCBIKEPSTAT_BOOL 狂野镖客", function() -- 狂野飙客
+    set_mass_packed_bool(unlock_state, 9361, 9553)
+end)
+
+packed_bool_unlock_menu:add_action("DLCGUNPSTAT_BOOL 进出口大亨", function() -- 进出口大亨
+    set_mass_packed_bool(unlock_state, 15369, 15561)
+end)
+
+packed_bool_unlock_menu:add_action("GUNTATPSTAT_BOOL 军火走私", function() -- 军火走私
+    set_mass_packed_bool(unlock_state, 15562, 15946)
+end)
+
+packed_bool_unlock_menu:add_action("DLCSMUGCHARPSTAT 走私大暴走", function() -- 走私大暴走
+    set_mass_packed_bool(unlock_state, 15946, 16010)
+    packed_funs.set_packed_bool(22069, false); -- 古邦800f cuban800
+end)
+
+packed_bool_unlock_menu:add_action("GANGOPSPSTAT_BOOL 末日豪劫", function() -- 末日豪劫
+    set_mass_packed_bool(unlock_state, 18098, 18162)
+end)
+
+packed_bool_unlock_menu:add_action("BUSINESSBATPSTAT_BOOL 夜总会DLC物品", function() -- 夜总会dlc
+    set_mass_packed_bool(unlock_state, 22066, 22194)
+    packed_funs.set_packed_bool(22069, false) -- 竞速 speedo4
+end)
+
+packed_bool_unlock_menu:add_action("ARENAWARSPSTAT_BOOL 竞技场DLC物品", function() -- 竞技场dlc
+    set_mass_packed_bool(unlock_state, 24962, 25538)
+end)
+
+packed_bool_unlock_menu:add_action("CASINOPSTAT_BOOL 钻石赌场DLC物品", function() -- 钻石赌场dlc
+    set_mass_packed_bool(unlock_state, 26810, 27258)
+end)
+
+packed_bool_unlock_menu:add_action("CASINOHSTPSTAT_BOOL 赌场抢劫DLC物品", function() -- 赌场抢劫dlc
+    set_mass_packed_bool(unlock_state, 28098, 28354)
+end)
+
+packed_bool_unlock_menu:add_action("HEIST3TATTOOSTAT_BOOL 赌场抢劫DLC物品", function() -- 赌场抢劫dlc
+    set_mass_packed_bool(unlock_state, 28355, 28483)
+end)
+
+packed_bool_unlock_menu:add_action("SU20PSTAT_BOOL夏季更新", function() -- 夏季更新
+    set_mass_packed_bool(unlock_state, 30355, 30483)
+end)
+
+packed_bool_unlock_menu:add_action("SU20TATTOOSTAT_BOOL 解锁夏季更新DLC物品", function() -- 夏季更新
+    set_mass_packed_bool(unlock_state, 30227, 30355)
+end)
+
+packed_bool_unlock_menu:add_action("HISLANDPSTAT_BOOL 解锁佩里克岛DLC物品", function() -- 佩里克岛抢劫dlc
+    set_mass_packed_bool(unlock_state, 30515, 30707)
+end)
+
+packed_bool_unlock_menu:add_action("TUNERPSTAT_BOOL 解锁车友会DLC物品", function() -- 车友会dlc
+    set_mass_packed_bool(unlock_state, 31707, 32283)
+end)
+
+packed_bool_unlock_menu:add_action("FIXERPSTAT_BOOL 解锁联系人DLC物品", function() -- 联系人dlc
+    set_mass_packed_bool(unlock_state, 32283, 32411)
+end)
+
+packed_bool_unlock_menu:add_action("FIXERTATTOOSTAT 解锁联系人DLC物品", function() -- 联系人dlc
+    set_mass_packed_bool(unlock_state, 32411, 32475)
+end)
+
+packed_bool_unlock_menu:add_action("Re-watch the dlc animation 重新观看动画", function()
+    packed_funs.set_packed_bool(28256, false) -- 安保確認
+    packed_funs.set_packed_bool(28257, false) -- 任务动画
+    packed_funs.set_packed_bool(22193, false) -- 改车铺进门動畫
+    packed_funs.set_packed_bool(31753, false) -- 改车铺开张任务動畫
+    packed_funs.set_packed_bool(31737, false) -- 车友会動畫
+    packed_funs.set_packed_bool(27089, false) -- 賭場動畫確認
+    packed_funs.set_packed_bool(27245, false) -- 平臺動畫
+    packed_funs.set_packed_bool(25008, false) -- 竞技场动画1
+    packed_funs.set_packed_bool(25009, false) -- 竞技场动画2
+    packed_funs.set_packed_bool(25010, false) -- 竞技场动画2
+end)
+
+----------------------------------packed int----------------------------------
+local packed_int_unlock_menu = packed_unlock_menu:add_submenu("Packed int 解锁") -- 二级菜单
+
+-- 冲冲猴旅行家
+packed_int_unlock_menu:add_action("冲冲猴旅行家", function()
+    packed_funs.set_packed_int(22063, 20)
+end)
+
+-- 恐霸批发价
+packed_int_unlock_menu:add_action("恐霸批发价", function()
+
+    packed_funs.set_packed_int(22050, 5)
+end)
+
+-- 商战黄金奖杯
+packed_int_unlock_menu:add_action("商战黄金奖杯", function()
+    if (packed_funs.get_packed_int(22058) < 20) then
+        packed_funs.set_packed_int(22058, 20)
+    end
+end)
+
+packed_int_unlock_menu:add_action("夜总会保险箱装饰", function()
+    packed_funs.set_packed_int(22051, 50); -- 木盒子
+    packed_funs.set_packed_int(22052, 100); -- 子弹铁盒
+    packed_funs.set_packed_int(22053, 20); -- 冰毒
+    packed_funs.set_packed_int(22054, 80); -- 大麻
+    packed_funs.set_packed_int(22055, 60); -- 伪证
+    packed_funs.set_packed_int(22056, 40); -- 假钞
+    packed_funs.set_packed_int(22057, 10); -- 可卡因
+end)
+
+packed_int_unlock_menu:add_action("设施维修区摆件", function()
+    packed_funs.set_packed_int(18982, 3) -- tM02
+    packed_funs.set_packed_int(18983, 3) -- 防暴车
+    packed_funs.set_packed_int(18984, 3) -- 导弹车
+    packed_funs.set_packed_int(18985, 3) -- 推进者
+    packed_funs.set_packed_int(18986, 3) -- 复仇者
+end)
+
+packed_int_unlock_menu:add_action("解锁双动左轮 使用后换占据再次使用", function()
+    if (packed_funs.get_packed_int(18981) == 0) then
+        packed_funs.set_packed_int(18981, 3) -- 双动左轮跳到最后任务阶段
+    end
+    if (packed_funs.get_packed_int(18981) > 3) then
+        packed_funs.set_packed_int(18981, 0) -- 双动左轮进度清零
+        -- 换占据后才能触发
+    end
+end)
+
+packed_int_unlock_menu:add_action("解锁石斧 使用后换占据再次使用", function()
+    if (packed_funs.get_packed_int(7315) == 0) then
+        packed_funs.set_packed_int(7315, 5); -- 跳到石斧最后任务阶段
+    end
+    if (packed_funs.get_packed_int(7315) > 5) then
+        packed_funs.set_packed_int(7315, 0); -- 双动左轮进度清零
+        -- 换占据后才能触发
+    end
+end)
